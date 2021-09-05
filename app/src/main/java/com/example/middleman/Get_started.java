288a234e9c615fa342602b8a6eb264268f794bbc
gaurangkeluskar22 <gaurangkeluskar22@gmail.com>
2021-09-06 02:18:31 +0530

    first commit

---
+++ app/src/main/java/com/example/middleman/Get_started.java
@@ -0,0 +1,14 @@
+package com.example.middleman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Get_started extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started);
+    }
+}
---
+++ app/src/main/java/com/example/middleman/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.middleman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button first = findViewById(R.id.first);
+        Button second = findViewById(R.id.second);
+        Button third = findViewById(R.id.third);
+        Button fourth = findViewById(R.id.fourth);
+
+        first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent first_intent = new Intent(MainActivity.this, Get_started.class);
+                startActivity(first_intent);
+            }
+        });
+
+        second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent second_intent = new Intent(MainActivity.this, Login.class);
+                startActivity(second_intent);
+            }
+        });
+
+        third.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent third_intent = new Intent(MainActivity.this, HomeScreen.class);
+                startActivity(third_intent);
+            }
+        });
+
+        fourth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fourth_intent = new Intent(MainActivity.this, Detail.class);
+                startActivity(fourth_intent);
+            }
+        });
+    }
+}
